d0d3a0dc02904a6203dd7342c5008da0781bc6b1
Mike-Bell <mikel3377@gmail.com>
2024-12-02 07:18:15 -0500

    Day2

--- 02/solution.js
+++ 02/solution.js
@@ -1,7 +1,51 @@
-const parseInput = input => input;
+const parseInput = input => input.split('\r\n').map(r => r.split(' ').map(Number));
 
-const runPart1 = input => input;
+const runPart1 = input => input.filter(row => {
+   let parity = 0;
+   for (let i = 0; i < row.length - 1; i++) {
+      const diff = row[i] - row[i + 1];
+      const newParity = diff > 0 ? 1 : -1;
+      if (i > 0 && parity !== newParity) {
+         return false;
+      }
+      parity = newParity;
 
-const runPart2 = input => input;
+      if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
+         return false;
+      }
+   }
+
+   return true;
+}).length;
+
+const runPart2 = input => input.filter(row => {
+   const isGood = thisRow => {
+      let parity = 0;
+      for (let i = 0; i < thisRow.length - 1; i++) {
+         const diff = thisRow[i] - thisRow[i + 1];
+         const newParity = diff > 0 ? 1 : -1;
+         if (i > 0 && parity !== newParity) {
+            return false;
+         }
+         parity = newParity;
+
+         if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
+            return false;
+         }
+      }
+      return true;
+   };
+
+   for (let i = -1; i < row.length; i++) {
+      let thisRow = row;
+      if (i > -1) {
+         thisRow = [...row.slice(0, i), ...row.slice(i + 1, row.length)];
+      }
+      if (isGood(thisRow)) {
+         return true;
+      }
+   }
+   return false;
+}).length;
 
 module.exports = {parseInput, runPart1, runPart2};
