795eda2fe09ad4141f20d08e1eb0ae09e3872118
Mike Bell <m.bell@techsmith.com>
2024-12-03 08:23:11 -0500

    Day 3 JS

--- 03/solution.js
+++ 03/solution.js
@@ -1,7 +1,24 @@
 const parseInput = input => input;
 
-const runPart1 = input => input;
+const runPart1 = input => {
+   const result = [...input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)];
+   return result.reduce((acc, curr) => {
+      return acc + Number(curr[1]) * Number(curr[2]);
+   }, 0);
+};
 
-const runPart2 = input => input;
+const runPart2 = input => {
+   const dos = [...input.matchAll(/do\(\)/g)].map(r => r.index).reverse();
+   const donts = [...input.matchAll(/don't\(\)/g)].map(r => r.index).reverse();
+   const result = [...input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)];
+   return result.reduce((acc, curr) => {
+      const lastDo = dos.find(i => i < curr.index);
+      const lastDont = donts.find(i => i < curr.index);
+      if (lastDont != null && (lastDo == null || lastDont > lastDo)) {
+         return acc;
+      }
+      return acc + Number(curr[1]) * Number(curr[2]);
+   }, 0);
+};
 
 module.exports = {parseInput, runPart1, runPart2};
