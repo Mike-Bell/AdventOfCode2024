8ccaa005bceb620467940238b2b16c213af96370
Mike-Bell <mikel3377@gmail.com>
2024-12-16 06:32:31 -0500

    Day 15 optimizations

--- 15/solution.js
+++ 15/solution.js
@@ -27,12 +27,16 @@ const runPart1 = input => {
    const maxR = map.length;
    const maxC = map[0].length;
 
-   for (let ri = 0; ri < maxR; ri++) {
+   // eslint-disable-next-line no-labels
+   outer: for (let ri = 0; ri < maxR; ri++) {
+      const row = map[ri];
       for (let ci = 0; ci < maxC; ci++) {
-         if (map[ri][ci] === '@') {
+         if (row[ci] === '@') {
             r = ri;
             c = ci;
-            map[ri][ci] = '.';
+            row[ci] = '.';
+            // eslint-disable-next-line no-labels
+            break outer;
          }
       }
    }
@@ -44,22 +48,24 @@ const runPart1 = input => {
       let ri = r + dr;
       let ci = c + dc;
 
-      if (map[ri][ci] === '#') {
+      let cur = map[ri][ci];
+      if (cur === '#') {
          continue;
       }
 
-      if (map[ri][ci] === '.') {
+      if (cur === '.') {
          r = ri;
          c = ci;
          continue;
       }
 
-      while (map[ri][ci] === 'O') {
+      while (cur === 'O') {
          ri += dr;
          ci += dc;
+         cur = map[ri][ci];
       }
 
-      if (map[ri][ci] === '#') {
+      if (cur === '#') {
          continue;
       }
 
@@ -70,9 +76,10 @@ const runPart1 = input => {
    }
 
    let sum = 0;
-   for (let ri = 0; ri < maxR; ri++) {
-      for (let ci = 0; ci < maxC; ci++) {
-         if (map[ri][ci] === 'O') {
+   for (let ri = 1; ri < maxR - 1; ri++) {
+      const row = map[ri];
+      for (let ci = 1; ci < maxC - 1; ci++) {
+         if (row[ci] === 'O') {
             sum += ri * 100 + ci;
          }
       }
@@ -85,8 +92,8 @@ const runPart2 = input => {
    const {map: inputMap, dirs} = input;
    let r = -1;
    let c = -1;
-   const maxR = inputMap.length;
-   const maxC = inputMap[0].length;
+   let maxR = inputMap.length;
+   let maxC = inputMap[0].length;
 
    const map = [];
 
@@ -118,7 +125,7 @@ const runPart2 = input => {
       const dr = DR[d];
       const dc = DC[d];
 
-      let ri = r + dr;
+      const ri = r + dr;
       let ci = c + dc;
 
       if (map[ri][ci] === '#') {
@@ -134,16 +141,16 @@ const runPart2 = input => {
       const horizontal = d === '<' || d === '>';
 
       if (horizontal) {
-         while (map[ri][ci] === '[' || map[ri][ci] === ']') {
-            ri += dr;
-            ci += dc;
+         let cur = map[ri][ci];
+         while (cur === '[' || cur === ']') {
+            ci += dc * 2;
+            cur = map[ri][ci];
          }
 
-         if (map[ri][ci] === '#') {
+         if (cur === '#') {
             continue;
          }
 
-         r += dr;
          c += dc;
          while (ci !== c) {
             map[ri][ci] = map[ri][ci - dc];
@@ -159,9 +166,9 @@ const runPart2 = input => {
       // eslint-disable-next-line no-labels
       outer: while (pushFront.length > 0) {
          const nextPushFront = [];
-         for (const p of pushFront) {
-            const nextL = map[p[0] + dr][p[1]];
-            const nextR = map[p[0] + dr][p[1] + 1];
+         for (const [pr, pc] of pushFront) {
+            const nextL = map[pr + dr][pc];
+            const nextR = map[pr + dr][pc + 1];
             if (nextL === '#' || nextR === '#') {
                pushes = [];
                // eslint-disable-next-line no-labels
@@ -169,13 +176,13 @@ const runPart2 = input => {
             }
 
             if (nextL === '[') {
-               nextPushFront.push([p[0] + dr, p[1]]);
+               nextPushFront.push([pr + dr, pc]);
             } else if (nextL === ']') {
-               nextPushFront.push([p[0] + dr, p[1] - 1]);
+               nextPushFront.push([pr + dr, pc - 1]);
             }
 
             if (nextR === '[') {
-               nextPushFront.push([p[0] + dr, p[1] + 1]);
+               nextPushFront.push([pr + dr, pc + 1]);
             }
          }
 
@@ -185,24 +192,27 @@ const runPart2 = input => {
 
       if (pushes.length > 0) {
          r = ri;
-         c = ci;
       }
 
       for (let i = pushes.length - 1; i >= 0; i--) {
-         const p = pushes[i];
-         if (map[p[0]][p[1]] === '[') {
-            map[p[0] + dr][p[1] + dc] = map[p[0]][p[1]];
-            map[p[0] + dr][p[1] + dc + 1] = map[p[0]][p[1] + 1];
-            map[p[0]][p[1]] = '.';
-            map[p[0]][p[1] + 1] = '.';
+         const [pr, pc] = pushes[i];
+         const cur = map[pr][pc];
+         if (cur === '[') {
+            map[pr + dr][pc] = cur;
+            map[pr + dr][pc + 1] = map[pr][pc + 1];
+            map[pr][pc] = '.';
+            map[pr][pc + 1] = '.';
          }
       }
    }
 
    let sum = 0;
-   for (let ri = 0; ri < map.length; ri++) {
-      for (let ci = 0; ci < map[0].length; ci++) {
-         if (map[ri][ci] === '[') {
+   maxR = map.length;
+   maxC = map[0].length;
+   for (let ri = 1; ri < maxR - 1; ri++) {
+      const row = map[ri];
+      for (let ci = 2; ci < maxC - 2; ci++) {
+         if (row[ci] === '[') {
             sum += ri * 100 + ci;
          }
       }
