ba0932d16ec77dd6c6534aff856d3505287cfb8f
Mike-Bell <mikel3377@gmail.com>
2024-12-22 08:15:22 -0500

    Day 22

--- 22/solution.js
+++ 22/solution.js
@@ -1,7 +1,48 @@
-const parseInput = input => input;
+/* eslint-disable no-bitwise */
+/* eslint-disable no-mixed-operators */
+const parseInput = input => input.split('\r\n').map(Number);
 
-const runPart1 = input => input;
+const runPart1 = input => input.reduce((acc, curr) => {
+   for (let i = 0; i < 2000; i++) {
+      curr = (((curr * 64) ^ curr) >>> 0) % 16777216;
+      curr = ((Math.floor(curr / 32) ^ curr) >>> 0) % 16777216;
+      curr = (((curr * 2048) ^ curr) >>> 0) % 16777216;
+   }
+   return acc + curr;
+}, 0);
 
-const runPart2 = input => input;
+const runPart2 = input => {
+   const BANANAS = {};
+   for (const val of input) {
+      let d0 = 0;
+      let d1 = 0;
+      let d2 = 0;
+      let d3 = 0;
+      let lastSecret = val;
+      const seen = new Set();
+      for (let i = 0; i < 2000; i++) {
+         let secret = lastSecret;
+         secret = (((secret * 64) ^ secret) >>> 0) % 16777216;
+         secret = ((Math.floor(secret / 32) ^ secret) >>> 0) % 16777216;
+         secret = (((secret * 2048) ^ secret) >>> 0) % 16777216;
+
+         d0 = d1;
+         d1 = d2;
+         d2 = d3;
+         d3 = (secret % 10) - (lastSecret % 10);
+         lastSecret = secret;
+
+         if (i > 2) {
+            const key = d0 * 1000000 + d1 * 10000 + d2 * 100 + d3;
+            if (!seen.has(key)) {
+               seen.add(key);
+               BANANAS[key] = (BANANAS[key] || 0) + (secret % 10);
+            }
+         }
+      }
+   }
+
+   return Math.max(...Object.values(BANANAS));
+};
 
 module.exports = {parseInput, runPart1, runPart2};
