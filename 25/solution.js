8ebe078f3722714fde2784a130c75d1911e0b06e
Mike-Bell <mikel3377@gmail.com>
2024-12-25 07:08:24 -0500

    Day 25

--- 25/solution.js
+++ 25/solution.js
@@ -1,7 +1,40 @@
-const parseInput = input => input;
+const parseInput = input => input.split('\r\n\r\n').map(chunk => chunk.split('\r\n'));
 
-const runPart1 = input => input;
+const runPart1 = input => {
+   const locks = [];
+   const keys = [];
+   for (const section of input) {
+      const isLock = section[0][0] === '#';
+      const geo = [];
+      for (let c = 0; c <= 4; c++) {
+         let n = -1;
+         for (let r = 0; r <= 6; r++) {
+            if (section[r][c] === '#') {
+               n++;
+            }
+            geo[c] = n;
+         }
+      }
 
-const runPart2 = input => input;
+      (isLock ? locks : keys).push(geo);
+   }
+
+   let sum = 0;
+   for (const key of keys) {
+      for (const lock of locks) {
+         sum++;
+         for (let c = 0; c <= 4; c++) {
+            if (key[c] + lock[c] > 5) {
+               sum--;
+               break;
+            }
+         }
+      }
+   }
+
+   return sum;
+};
+
+const runPart2 = () => 'MERRY CHRISTMAS';
 
 module.exports = {parseInput, runPart1, runPart2};
